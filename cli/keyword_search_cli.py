b8e4edc02f8387022360f2890cf3af155720b6ec
gutek00714 <guciolaczkowski1@gmail.com>
2026-03-12 04:45:18 +0100

    Add InvertedIndex class with build, save, and search commands

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.keyword_search import keyword_search
+from lib.keyword_search import keyword_search, InvertedIndex
 
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    subparsers.add_parser("build", help="Build the inverted index")
 
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
@@ -18,6 +19,12 @@ def main() -> None:
         case "search":
             print(f"Searching for: {args.query}")
             keyword_search(args.query)
+        case "build":
+            idx = InvertedIndex()
+            idx.build()
+            idx.save()
+            docs = idx.get_documents("merida")
+            print(f"First document for 'merida': {docs[0]}")
         case _:
             parser.print_help()
 
--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,16 +1,53 @@
-import json
+import os
+import pickle
 import string
+from lib import search_utils
 from nltk.stem import PorterStemmer
+
+class InvertedIndex:
+    def __init__(self):
+        self.index = {}
+        self.docmap = {}
+
+    def __add_document(self, doc_id, text):
+        tokens = tokenize(text)
+        for token in tokens:
+            if token in self.index:
+                self.index[token].add(doc_id)
+            else:
+                self.index[token] = {doc_id}
+
+    def get_documents(self, term):
+        term = term.lower()
+        ids = self.index.get(term, set())
+        return sorted(ids)
+    
+    def build(self):
+        movies = search_utils.load_movies()
+        for movie in movies:
+            doc_id = movie["id"]
+            self.docmap[doc_id] = movie
+            self.__add_document(doc_id, f"{movie['title']} {movie['description']}")
+
+    def save(self):
+        # Check if cache dir exist -> create one
+        if not os.path.exists('cache'):
+            os.makedirs('cache')
+
+        # Save index data (wb - write mode)
+        with open('cache/index.pkl', 'wb') as f:
+            pickle.dump(self.index, f)
+
+        # Save docmap data (wb - write mode)
+        with open('cache/docmap.pkl', 'wb') as f:
+            pickle.dump(self.docmap, f)
 
 def keyword_search(query):
     # Load movie data from JSON storage
-    with open('data/movies.json', 'r') as f:
-        data = json.load(f)
+    movies_list = search_utils.load_movies()
 
     stemmer = PorterStemmer()
-    movies_list = data["movies"]
     results = []
-    i=1
 
     # Preprocess the user query into stemmed roots
     query_tokens = [stemmer.stem(token) for token in remove_stopwords(tokenize(query))]
@@ -22,12 +59,14 @@ def keyword_search(query):
         # Check if any stemmed query word exists within the stemmed movie title tokens
         if any(q in t for q in query_tokens for t in title_tokens):
             results.append(item)
-            print(f"{i}. {item['title']}")
-            i+=1
 
         # Limit retults
         if len(results) == 5:
             break
+
+        # Print the result
+        for i, item in enumerate(results, start=1):
+            print(f"{i}. {item['title']}")
 
     return results
 
@@ -35,9 +74,6 @@ def tokenize(text):
     cleaned_text = text.lower().translate(str.maketrans('', '', string.punctuation))
     return cleaned_text.split()
 
-def remove_stopwords(text):
-    with open("data/stopwords.txt") as f:
-        words = f.read()
-
-    words = words.splitlines()
-    return [word for word in text if word not in words]
+def remove_stopwords(tokens):
+    stopwords = search_utils.load_stopwords()
+    return [word for word in tokens if word not in stopwords]
