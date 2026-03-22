40edbef5e81e58eca27cd92dc30c17a691098a7b
gutek00714 <guciolaczkowski1@gmail.com>
2026-03-22 23:52:50 +0100

    Add weighted combination

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -1,6 +1,7 @@
 import argparse
 
-from lib.hybrid_search import normalize
+from lib.hybrid_search import HybridSearch, normalize
+from lib.search_utils import load_movies
 
 
 def main() -> None:
@@ -10,6 +11,13 @@ def main() -> None:
     normalize_parser = subparsers.add_parser("normalize", help="Normalnize BM25 and Cosine score")
     normalize_parser.add_argument("scores", nargs="+", type=float, help="List of scores")
 
+    weighted_search = subparsers.add_parser("weighted-search", help="Combine keyword(BM25) and semantic scores")
+    weighted_search.add_argument("query", type=str, help="Search query")
+    weighted_search.add_argument("--alpha", type=float, default=0.5, help="Alpha constant")
+    weighted_search.add_argument("--limit", type=int, default=5, help="Limit query")
+
+    rrf_search = subparsers.add_parser("rrf-search", help=)
+
     args = parser.parse_args()
 
     match args.command:
@@ -17,6 +25,17 @@ def main() -> None:
             scores = normalize(args.scores)
             for score in scores:
                 print(f"* {score:.4f}")
+        case "weighted-search":
+            movies = load_movies()
+            hs = HybridSearch(movies)
+            search = hs.weighted_search(args.query, args.alpha, args.limit)
+            for i, item in enumerate(search, start=1):
+                print(f"{i}. {item['doc']['title']}")
+                print(f"Hybrid Score: {item['hybrid_score']:.3f}")
+                print(f"BM25: {item['keyword_score']:.3f}, Semantic: {item['semantic_score']:.3f}")
+                print(f"{item['doc']['description'][:100]}...")
+        case "rrf-search":
+
         case _:
             parser.print_help()
 
--- cli/lib/hybrid_search.py
+++ cli/lib/hybrid_search.py
@@ -11,23 +11,64 @@ class HybridSearch:
         self.semantic_search.load_or_create_chunk_embeddings(documents)
 
         self.idx = InvertedIndex()
-        if not os.path.exists(self.idx.index_path):
+        if not os.path.exists("cache/index_pkl"):
             self.idx.build()
             self.idx.save()
+        else:
+            self.idx.load()
 
     def _bm25_search(self, query, limit):
         self.idx.load()
         return self.idx.bm25_search(query, limit)
 
     def weighted_search(self, query, alpha, limit=5):
-        raise NotImplementedError("Weighted hybrid search is not implemented yet.")
+        # get bm25 results
+        bm25 = self._bm25_search(query, limit * 500)
+
+        # get semantic results
+        semantic = self.semantic_search.search_chunks(query, limit * 500)
+
+        # combine all the results
+        # -----------------------
+        bm25_scores = normalize([score for _, _, score in bm25])
+        semantic_scores = normalize([item["score"] for item in semantic])
+
+        # map id to the document with its keyword and semantic search
+        combined = {}
+
+        for i, (doc_id, title, _) in enumerate(bm25):
+            combined[doc_id] = {
+                "doc": self.idx.docmap[doc_id],
+                "keyword_score": bm25_scores[i],
+                "semantic_score": 0.0,
+            }
+        
+        for i, item in enumerate(semantic):
+            doc_id = item["id"]
+            if doc_id in combined:
+                combined[doc_id]["semantic_score"] = semantic_scores[i]
+            else:
+                combined[doc_id] = {
+                    "doc": self.documents[item["id"]] if isinstance(self.documents, dict) else next(d for d in self.documents if d["id"] == doc_id),
+                    "keyword_score": 0.0,
+                    "semantic_score": semantic_scores[i],
+                }
+
+        # add hybrid score to each document
+        for doc_id in combined:
+            combined[doc_id]["hybrid_score"] = (alpha * combined[doc_id]["keyword_score"] + (1 - alpha) * combined[doc_id]["semantic_score"])
+        #--------------------
+
+        sorted_results = sorted(combined.values(), key=lambda x: x["hybrid_score"], reverse=True)
+        return sorted_results[:limit]
+
 
     def rrf_search(self, query, k, limit=10):
         raise NotImplementedError("RRF hybrid search is not implemented yet.")
     
 def normalize(scores) -> list:
     if len(scores) == 0:
-        return
+        return []
     
     # find max and min score in a list
     max_score = max(scores)
